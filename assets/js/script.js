39197ee9a3d21e36e623d78e8a61bd0d02849a5a
Noah Becker <102703954+noah35becker@users.noreply.github.com>
2022-06-28 13:30:53 -0400

    User class created

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,4 @@
 
-class Question{
-    constructor(questionText, ...choiceObjects){
-        this.text = questionText;
-        this.choices = choiceObjects;
-    }
-}
-
 class Choice{
     constructor(choiceText, rightAns){
         this.text = choiceText;
@@ -18,5 +11,30 @@ class Choice{
 
     isRight(){
         return this.rightAnswer;
+    }
+}
+
+
+class Question{
+    constructor(questionText, ...choiceObjects){
+        this.text = questionText;
+        this.choices = choiceObjects;
+    }
+}
+
+
+class User{
+    constructor(username, pw){
+        username += '';
+        this.username = username.toLowerCase();
+        this.password = pw;
+        this.highScore = 0;
+    }
+
+    setHighScore(newScore){
+        if (newScore > this.highScore)
+            this.highScore = newScore;
+
+        return this.highScore;
     }
 }
