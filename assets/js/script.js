1bff088f795d4e05ee03481cd63d2705817e5381
Noah Becker <102703954+noah35becker@users.noreply.github.com>
2022-06-28 21:11:24 -0400

    added login form elements

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,50 @@
 
-var pageContent = document.querySelector('main');
+const pageContent = document.querySelector('main');
+
+const newUserBtn = document.createElement('button');
+    newUserBtn.textContent = 'New user';
+    newUserBtn.classList.add('btn', 'login-btn');
+
+const returningUserBtn = document.createElement('button');
+    returningUserBtn.textContent = 'Returning user';
+    returningUserBtn.classList.add('btn', 'login-btn');
+
+const newUserForm = document.createElement('form');
+    newUserForm.setAttribute('id', 'new-user-form');
+    newUserForm.className = 'login-form';
+
+const returningUserForm = document.createElement('form');
+    returningUserForm.setAttribute('id', 'returning-user-form');
+    returningUserForm.className = 'login-form';
+
+const usernameLabel = document.createElement('label');
+    usernameLabel.setAttribute('for', 'username-input');
+    usernameLabel.textContent = 'Username:';
+
+const usernameInput = document.createElement('input');
+    usernameInput.setAttribute('type', 'text');
+    usernameInput.setAttribute('name', 'username-input');
+    usernameInput.setAttribute('placeholder', 'username');
+
+const passwordLabel1 = document.createElement('label');
+    passwordLabel1.setAttribute('for', 'password-input-1');
+    passwordLabel1.textContent = 'Password:';
+
+const passwordInput1 = document.createElement('input');
+    passwordInput1.setAttribute('type', 'password');
+    passwordInput1.setAttribute('name', 'password-input-1');
+    passwordInput1.setAttribute('placeholder', 'password');
+
+const passwordLabel2 = document.createElement('label');
+    passwordLabel2.setAttribute('for', 'password-input-2');
+    passwordLabel2.textContent = 'Re-enter password:';
+
+const passwordInput2 = passwordInput1.cloneNode();
+    passwordInput2.setAttribute('name', 'password-input-2')
+
+var loginSubmitBtn = document.createElement('button');
+// Later, set the 'form' attribute to match the given form's id
+
 
 
 class Choice{
@@ -43,18 +88,24 @@ class User{
 }
 
 
+function pageClear(){
+    pageContent.innerHTML = '';
+}
+
 
 function showLoginScreen(){
-    var newUserBtn = document.createElement('button');
-    newUserBtn.textContent = 'New user';
-    newUserBtn.classList.add('btn', 'login-btn');
-
-    var returningUserBtn = document.createElement('button');
-    returningUserBtn.textContent = 'Returning user';
-    returningUserBtn.classList.add('btn', 'login-btn');
+    pageClear();
 
     pageContent.appendChild(newUserBtn);
     pageContent.appendChild(returningUserBtn);
 }
+
+
+function showNewUserScreen(){
+    pageClear();
+
+    
+}
+
 
 showLoginScreen();
