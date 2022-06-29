03de8901a76c594953698892d060dc95b49cfbb6
Noah Becker <102703954+noah35becker@users.noreply.github.com>
2022-06-28 21:47:18 -0400

    show password checkbox added, some form elements now required

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,31 +12,51 @@ const returningUserBtn = document.createElement('button');
 const newUserForm = document.createElement('form');
     newUserForm.id = 'new-user-form';
     newUserForm.className = 'login-form';
-    newUserForm.textContent = 'Enter new user info'
+    newUserForm.textContent = 'Enter new user info';
 
 const returningUserForm = document.createElement('form');
     returningUserForm.id = 'returning-user-form';
     returningUserForm.className = 'login-form';
-    returningUserForm.textContent = 'Enter login info'
+    returningUserForm.textContent = 'Enter login info';
 
 const usernameInput = document.createElement('input');
     usernameInput.setAttribute('type', 'text');
     usernameInput.setAttribute('name', 'username-input');
     usernameInput.setAttribute('placeholder', 'username');
+    usernameInput.setAttribute('required','');
 
 const passwordInput1 = document.createElement('input');
     passwordInput1.setAttribute('type', 'password');
     passwordInput1.setAttribute('name', 'password-input-1');
     passwordInput1.setAttribute('placeholder', 'password');
+    passwordInput1.setAttribute('required','');
 
 const passwordInput2 = document.createElement('input');
     passwordInput2.setAttribute('type', 'password');
     passwordInput2.setAttribute('name', 'password-input-1');
     passwordInput2.setAttribute('placeholder', 're-enter password');
+    passwordInput2.setAttribute('required','');
+
+const showPasswordCheckbox = document.createElement('input');
+    showPasswordCheckbox.setAttribute('type', 'checkbox');
+    showPasswordCheckbox.setAttribute('name', 'show-password');
+
+const showPasswordLabel = document.createElement('label');
+    showPasswordLabel.setAttribute('for', 'show-password');
+    showPasswordLabel.textContent = 'Show password';
+
+const confirmTermsCheckbox = document.createElement('input');
+    confirmTermsCheckbox.setAttribute('type', 'checkbox');
+    confirmTermsCheckbox.setAttribute('name', 'confirm-terms');
+    confirmTermsCheckbox.setAttribute('required', '');
+
+const confirmTermsLabel = document.createElement('label');
+    confirmTermsLabel.setAttribute('for', 'confirm-terms');
+    confirmTermsLabel.textContent = 'I confirm that this username/pw are not stored securely'; //UPDATE THIS TEXT
 
 var loginSubmitBtn = document.createElement('button');
 // Later, set the 'form' attribute to match the given form's id
-// Later, set the textContent to 'Register' or 'Login'
+// Later, set the textContent to either 'Register' or 'Login'
 
 
 
@@ -99,6 +119,10 @@ function showNewUserScreen(){
 
     newUserForm.appendChild(usernameInput);
     newUserForm.appendChild(passwordInput1);
+    newUserForm.appendChild(showPasswordCheckbox);
+    newUserForm.appendChild(showPasswordLabel);
+    newUserForm.appendChild(confirmTermsCheckbox);
+    newUserForm.appendChild(confirmTermsLabel);
 
     loginSubmitBtn.setAttribute('form', newUserForm.id);
     loginSubmitBtn.textContent = 'Register';
@@ -114,6 +138,8 @@ function showReturningUserScreen(){
     returningUserForm.appendChild(usernameInput);
     returningUserForm.appendChild(passwordInput1);
     returningUserForm.appendChild(passwordInput2);
+    returningUserForm.appendChild(showPasswordCheckbox);
+    returningUserForm.appendChild(showPasswordLabel);
 
     loginSubmitBtn.setAttribute('form', returningUserForm.id);
     loginSubmitBtn.textContent = 'Login';
@@ -123,4 +149,4 @@ function showReturningUserScreen(){
 }
 
 
-showReturningUserScreen();
+showNewUserScreen();
